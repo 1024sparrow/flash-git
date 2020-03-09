ca8d4b0ddb036394f188b9b9868f6ca6ea1d57ad
1024sparrow <1024sparrow@gmail.com>
2020-03-09 07:05:02 +0300

    123

--- install.sh
+++ install.sh
@@ -12,19 +12,18 @@ then
 	exit 1
 fi
 
-#if [ ! $(id -u) -eq 0 ]
-#then
-#    echo Run this under ROOT only!
-#    exit 1
-#fi
-#exit 0
+if [ ! $(id -u) -eq 0 ]
+then
+    echo Run this under ROOT only!
+    exit 1
+fi
 
-#echo > /usr/share/flash-git/hardware
-#for i in "E: ID_VENDOR_ID=" "E: ID_MODEL_ID=" "E: DEVTYPE=" "E: ID_SERIAL="
-#do
-#var=$(udevadm info --export --name $1 | grep "$i")
-#echo ${i:3}${var:${#i}} >> /usr/share/flash-git/hardware
-#done
+echo > /usr/share/flash-git/hardware
+for i in "E: ID_VENDOR_ID=" "E: ID_MODEL_ID=" "E: DEVTYPE=" "E: ID_SERIAL="
+do
+var=$(udevadm info --export --name $1 | grep "$i")
+echo ${i:3}${var:${#i}} >> /usr/share/flash-git/hardware
+done
 
 source /usr/share/flash-git/hardware
 echo $ID_SERIAL
@@ -37,8 +36,11 @@ do
 	echo $i
 	git clone --bare --shared "$i"/.git root/$(basename $i).git
 done
+cp -L $2 root/repos
 
-#mkfs.ext4 $1 -d root
+mkfs.ext4 $1 -d root && echo OK || echo FAILED
+rm -rf root
+echo FINISHED
 
 
 #mkdir /usr/share/flash-git
